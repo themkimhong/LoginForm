fe33d7726cedd54efef888b3c1b3e570a9170005
themkimhong <themkimhong2@gmail.com>
2021-06-28 11:31:12 +0700

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/App';
+import "./styles/index.css";
+import reportWebVitals from './others/reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+reportWebVitals();
